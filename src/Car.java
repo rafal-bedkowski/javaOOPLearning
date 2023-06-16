ebb78f37ca9967b6031d838ac80024885ce3efad
Rafal Bedkowski <rafalbedkowski.1992@wp.pl>
2023-06-16 12:50:42 +0200

    first commit learning OOP JAVA clas, object, encapsulation, getter/setter

---
+++ src/Car.java
@@ -0,0 +1,57 @@
+public class Car {
+
+    private String make;
+    private String model;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    private String color;
+    private int doors;
+    private boolean convertible;
+
+    public void describeCar() {
+        System.out.println(doors + "-doors " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible " : " "));
+    }
+}
